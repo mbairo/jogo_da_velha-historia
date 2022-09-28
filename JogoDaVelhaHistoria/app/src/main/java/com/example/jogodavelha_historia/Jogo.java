ac50bdc8181ae96f8c149197fac03c92d87d9579
mbairo <bairo.marcos@gmail.com>
2022-09-28 13:18:00 -0300

    jogo completado - ainda com erro ao finalizar

--- JogoDaVelhaHistoria/app/src/main/java/com/example/jogodavelha_historia/Jogo.java
+++ JogoDaVelhaHistoria/app/src/main/java/com/example/jogodavelha_historia/Jogo.java
@@ -1,3 +1,4 @@
+
 package com.example.jogodavelha_historia;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -32,6 +33,10 @@ public class Jogo extends AppCompatActivity {
     private String ganhador;
     private String p1;
     private String p2;
+
+    private ImageView imageView_p1;
+    private ImageView imageView_p2;
+
     private int player;
     private  int contador;
 
@@ -139,96 +144,141 @@ public class Jogo extends AppCompatActivity {
 
                 // Jogador 1 (p1)
                 String heroiEscolhidoP1 = editText_num_jogador1.getText().toString();
+
                 if (heroiEscolhidoP1.equals("1")) {
                     img_p1.setImageResource(R.drawable.ayrtonsenna);
-                    img_p1_x = img_p1;
                 } else if (heroiEscolhidoP1.equals("2")) {
                     img_p1.setImageResource(R.drawable.dompedro2);
-                    img_p1_x = img_p1;
                 } else if (heroiEscolhidoP1.equals("3")) {
-                    img_p1.setImageResource(R.drawable.santosdumont);
-                    img_p1_x = img_p1;
-                } else if (heroiEscolhidoP1.equals("4")) {
                     img_p1.setImageResource(R.drawable.ruibarbosa);
-                    img_p1_x = img_p1;
+                } else if (heroiEscolhidoP1.equals("4")) {
+                    img_p1.setImageResource(R.drawable.santosdumont);
                 } else if (heroiEscolhidoP1.equals("5")){
+                    img_p1.setImageResource(R.drawable.tiradentes);
+                } else if (heroiEscolhidoP1.equals("6")){
+                    img_p1.setImageResource(R.drawable.josebonifacio);
+                } else if (heroiEscolhidoP1.equals("7")){
                     img_p1.setImageResource(R.drawable.chapolin);
-                    img_p1_x = img_p1;
                 }
 
                 // Jogador 2 (p2)
                 String heroiEscolhidoP2 = editText_num_jogador2.getText().toString();
+
                 if (heroiEscolhidoP2.equals("1")) {
                     img_p2.setImageResource(R.drawable.ayrtonsenna);
-                    img_p2_o = img_p2;
                 } else if (heroiEscolhidoP2.equals("2")) {
                     img_p2.setImageResource(R.drawable.dompedro2);
-                    img_p2_o = img_p2;
                 } else if (heroiEscolhidoP2.equals("3")) {
-                    img_p2.setImageResource(R.drawable.santosdumont);
-                    img_p2_o = img_p2;
-                } else if (heroiEscolhidoP2.equals("4")) {
                     img_p2.setImageResource(R.drawable.ruibarbosa);
-                    img_p2_o = img_p2;
+                } else if (heroiEscolhidoP2.equals("4")) {
+                    img_p2.setImageResource(R.drawable.santosdumont);
                 } else if (heroiEscolhidoP2.equals("5")){
+                    img_p2.setImageResource(R.drawable.tiradentes);
+                } else if (heroiEscolhidoP2.equals("6")){
+                    img_p2.setImageResource(R.drawable.josebonifacio);
+                } else if (heroiEscolhidoP2.equals("7")){
                     img_p2.setImageResource(R.drawable.chapolin);
-                    img_p2_o.setImageResource(R.drawable.chapolin);
                 }
             }
         });
     }
 
-    // Metodos
-    private void BuscarElementos () {
-        btn_voltar = findViewById(R.id.btn_voltar);
-        btn_check = findViewById(R.id.btn_check);
-        linha0_coluna0 = findViewById(R.id.linha0_coluna0);
-        edit_linha1_coluna0 = findViewById(R.id.edit_linha1_coluna0);
-        img_p1 = findViewById(R.id.img_p1);
-        img_p2 = findViewById(R.id.img_p2);
-
-        editText_num_jogador1 = findViewById(R.id.editText_num_jogador1);
-        editText_num_jogador2 = findViewById(R.id.editText_num_jogador2);
-    }
-
     public void jogada (ImageView campo, int linhaX, int colunaY) {
-        if (player == 1) {
-            matriz[linhaX][colunaY] = 1;
-            campo = img_p1_x;
-            player = 2;
-            ganhador = p1;
-            mostrarGanhador(1);
-        } else {
-            matriz[linhaX][colunaY] = 2;
-            campo = img_p2_o;
-            player = 1;
-            ganhador = p2;
-            mostrarGanhador(2);
+        String heroiEscolhidoP1 = editText_num_jogador1.getText().toString();
+        String heroiEscolhidoP2 = editText_num_jogador2.getText().toString();
 
+        if (player == 1) {
+            if (heroiEscolhidoP1.equals("1")){
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.ayrtonsenna);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("2")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.dompedro2);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("3")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.ruibarbosa);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("4")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.santosdumont);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("5")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.tiradentes);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("6")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.josebonifacio);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            } else if (heroiEscolhidoP1.equals("7")) {
+                matriz[linhaX][colunaY] = 1;
+                campo.setImageResource(R.drawable.chapolin);
+                player = 2;
+                ganhador = p1;
+                mostrarGanhador(1);
+            }
+
+
+        } else {
+            if (heroiEscolhidoP2.equals("2")){
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.ayrtonsenna);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("2")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.dompedro2);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("3")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.ruibarbosa);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("4")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.santosdumont);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("5")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.tiradentes);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("6")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.josebonifacio);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            } else if (heroiEscolhidoP2.equals("7")) {
+                matriz[linhaX][colunaY] = 2;
+                campo.setImageResource(R.drawable.chapolin);
+                player = 1;
+                ganhador = p2;
+                mostrarGanhador(2);
+            }
         }
         contador++;
-    }
-    public boolean fimDeJogo (int posicaoClicada) {
-        for (int indice = 0; indice < matriz.length; indice++) {
-            if (matriz[indice][0] == posicaoClicada && matriz[indice][1] == posicaoClicada && matriz[indice][2] == posicaoClicada) {
-                empate = false;
-                return true;
-            }
-            if (matriz[0][indice] == posicaoClicada && matriz[1][indice] == posicaoClicada && matriz[2][indice] == posicaoClicada) {
-                empate = false;
-                return true;
-            }
-        }
-        if (matriz[0][0] == posicaoClicada && matriz[1][1] == posicaoClicada && matriz[2][2] == posicaoClicada) {
-            empate = false;
-            return true;
-        }
-        if (matriz[2][0] == posicaoClicada && matriz[1][1] == posicaoClicada && matriz[0][2] == posicaoClicada) {
-            empate = false;
-            return true;
-        }
-
-        return false;
     }
 
     public void mostrarGanhador(int x) {
@@ -236,30 +286,53 @@ public class Jogo extends AppCompatActivity {
             AlertDialog.Builder alertaVencedor = new AlertDialog.Builder(this);
             alertaVencedor.setTitle("VITORIA");
             alertaVencedor.setMessage(player);
+
             alertaVencedor.setPositiveButton("ok", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     // todo
                 }
-
             });
             alertaVencedor.create();
             alertaVencedor.show();
         }
-
     }
 
+    public boolean fimDeJogo (int heroiDoPlayer) {
+        for (int indice = 0; indice < matriz.length; indice++) {
+            if (matriz[indice][0] == heroiDoPlayer && matriz[indice][1] == heroiDoPlayer && matriz[indice][2] == heroiDoPlayer) {
+                empate = false;
+                return true;
+            }
+            if (matriz[0][indice] == heroiDoPlayer && matriz[1][indice] == heroiDoPlayer && matriz[2][indice] == heroiDoPlayer) {
+                empate = false;
+                return true;
+            }
+        }
+        if (matriz[0][0] == heroiDoPlayer && matriz[1][1] == heroiDoPlayer && matriz[2][2] == heroiDoPlayer) {
+            empate = false;
+            return true;
+        }
+        if (matriz[2][0] == heroiDoPlayer && matriz[1][1] == heroiDoPlayer && matriz[0][2] == heroiDoPlayer) {
+            empate = false;
+            return true;
+        }
+        return false;
+    }
+
+    // Metodos
+    private void BuscarElementos () {
+        btn_voltar = findViewById(R.id.btn_voltar);
+        btn_check = findViewById(R.id.btn_check);
+        linha0_coluna0 = findViewById(R.id.linha0_coluna0);
+        img_p1 = findViewById(R.id.img_p1);
+        img_p2 = findViewById(R.id.img_p2);
+        editText_num_jogador1 = findViewById(R.id.editText_num_jogador1);
+        editText_num_jogador2 = findViewById(R.id.editText_num_jogador2);
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
-
 
 
 
